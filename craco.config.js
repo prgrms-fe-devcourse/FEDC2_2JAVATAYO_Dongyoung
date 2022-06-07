e67cbe085d5f9e32c97b00e049d218a4d5695b8e
YuHyun <puh0128@gmail.com>
2022-06-08 00:44:59 +0900

    init project

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+module.exports = {
+  babel: {
+    presets: ["@emotion/babel-preset-css-prop"],
+  },
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
+};
