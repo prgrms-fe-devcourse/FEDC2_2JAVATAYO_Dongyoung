cd1a4ea84407aadbafac6ff5f4bbfe1d9fd21b6f
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-06-09 21:16:40 +0900

    ✨ feat: Select 구현 (#61)
    * ✨ feat: Select 구현
    
    * :lipstick: style: 필요없는 주석 삭제

---
+++ src/components/create/Select/Select.tsx
@@ -0,0 +1,41 @@
+import React, { useState, Dispatch, SetStateAction } from "react";
+import "antd/dist/antd.min.css";
+import { Select as AntdSelect } from "antd";
+const { Option } = AntdSelect;
+
+interface SelectInterface {
+  defaultValue: string;
+  options: Array<object>;
+  canAllowClear: boolean;
+  setSelectedValue: Dispatch<SetStateAction<string>>;
+}
+
+const Select: React.FC<SelectInterface> = ({
+  defaultValue,
+  options,
+  canAllowClear,
+  setSelectedValue
+}) => {
+  const handleChange = (value: string) => {
+    setSelectedValue(value);
+  };
+
+  return (
+    <AntdSelect
+      defaultValue={defaultValue}
+      style={{
+        width: "120px"
+      }}
+      onChange={handleChange}
+      allowClear={canAllowClear}
+    >
+      {options.map((it: { id: number; value: string; label: string }) => (
+        <Option key={it.id} value={it.value}>
+          {it.label}
+        </Option>
+      ))}
+    </AntdSelect>
+  );
+};
+
+export default Select;
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import Select from "../components/create/Select";
+
 const Create: React.FC = () => {
-  return <div>Create</div>;
+  //todo : type으로 빼야함 + value 형진님과 논의!
+  const options = [
+    { id: 1, value: "online", label: "온라인" },
+    { id: 2, value: "offline", label: "오프라인" },
+    { id: 3, value: "both", label: "온오프라인 병행" },
+    { id: 4, value: "notyet", label: "미정" }
+  ];
+
+  const [selectedValue, setSelectedValue] = useState("online");
+
+  return (
+    <div>
+      <Select
+        defaultValue={"온라인"}
+        options={options}
+        canAllowClear={true}
+        setSelectedValue={setSelectedValue}
+      />
+    </div>
+  );
 };
 export default Create;
