a7e1710e1a67d7b7e18d439f6c987e0b2300bb83
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-06-10 23:19:24 +0900

    :sparkles: feat: Cascader구현 (#73)
    * :sparkles: feat: Cascader구현
    
    * :lipstick: style: 불필요한 주석 삭제

---
+++ src/components/create/Cascader/Cascader.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Cascader as AntdCascader } from "antd";
+
+interface CascaderInterface {
+  isMultiple: boolean;
+  options: Array<object>;
+  setSelectedValues: React.Dispatch<React.SetStateAction<any[]>>;
+}
+
+const Cascader: React.FC<CascaderInterface> = ({
+  isMultiple,
+  options,
+  setSelectedValues
+}) => {
+  const onChange = (value) => {
+    setSelectedValues(value);
+  };
+
+  return (
+    <>
+      <AntdCascader
+        multiple={isMultiple}
+        style={{
+          width: "100%"
+        }}
+        options={options}
+        onChange={onChange}
+        maxTagCount="responsive"
+      />
+    </>
+  );
+};
+export default Cascader;
