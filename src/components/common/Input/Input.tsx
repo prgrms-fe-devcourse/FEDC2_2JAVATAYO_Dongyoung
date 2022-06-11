5fb5dc2f71656c1ef5d448762f5b33fa97ba5fe7
Jenet <xuanchen0910@gmail.com>
2022-06-12 04:19:38 +0900

    ✨ feat: Input Props 추가 * name, value 추가 * width 기본 400px으로 설정

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -2,24 +2,30 @@ import * as S from "./style";
 
 interface InputInterface {
   type?: string;
-  width: number;
+  width?: number;
   placeholder?: string;
+  value: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
+  name?: string;
 }
 
 const Input: React.FC<InputInterface> = ({
   type,
-  width,
+  width = 400,
   placeholder,
+  value,
   onChange,
+  name,
   ...props
 }) => {
   return (
     <S.Input
-      type={type}
+      type={type ? type : "text"}
       style={{ width }}
-      placeholder={placeholder}
+      placeholder={placeholder ? placeholder : ""}
+      value={value}
       onChange={onChange}
+      name={name ? name : ""}
       {...props}
     />
   );
