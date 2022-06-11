9baac2e8d465912ce4dde6a56673acc35d3654e3
NamgyungKim <namgyung.kim@gmail.com>
2022-06-11 15:47:33 +0900

    💄 style: 임시 header footer 추가

--- src/components/common/AppLayout/AppLayout.tsx
+++ src/components/common/AppLayout/AppLayout.tsx
@@ -6,7 +6,13 @@ interface AppLayoutInterface {
 
 const AppLayout: React.FC<AppLayoutInterface> = ({ children }) => {
   // header, footer 컴포넌트 추가
-  return <S.Layout>{children}</S.Layout>;
+  return (
+    <>
+      <S.Header />
+      <S.Layout>{children}</S.Layout>
+      <S.Footer />
+    </>
+  );
 };
 
 export default AppLayout;
--- src/components/common/AppLayout/style.tsx
+++ src/components/common/AppLayout/style.tsx
@@ -9,3 +9,12 @@ export const Layout = styled.div`
     box-sizing: border-box;
   }
 `;
+
+export const Header = styled.div`
+  height: 60px;
+  background-color: #f5f5f5;
+`;
+export const Footer = styled.div`
+  height: 196px;
+  background-color: #f5f5f5;
+`;
