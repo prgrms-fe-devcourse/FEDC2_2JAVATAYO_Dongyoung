ed5bf05c4c2ec87ef5e4d4fffb61a1671330e867
hwoo3303 <hwoo3303@naver.com>
2022-06-14 03:19:09 +0900

    ✨ feat: Button 공통 컴포넌트 구현 (#80)
    * ✨ feat: Button 컴포넌트 초기 구현(피그마 스타일 미적용)
    
    * ✨ feat: Button 컴포넌트 구현
    
    * ✨ feat: Button 공통 컴포넌트 구현
    
    * ✨ feat: Button 공통 컴포넌트 구현
    
    * ✨ feat: Button 공통 컴포넌트 props 및 style 변경
    
    * ✨ feat: Button 공통 컴포넌트 props 및 style 변경
    
    * 💄 style: 버튼 font, background 디자인 가이드 색상으로 변경
    
    * 💄 style: 버튼 font, background 디자인 가이드 색상으로 변경
    
    * ✨ feat: Button 컴포넌트 옵셔널처리, 구조분해 할당으로 변경
    
    * ✨ feat: Button 컴포넌트 disabled 스타일 추가
    
    * ✨ feat: Button 컴포넌트 label제거 및 children 추가
    
    * ✨ feat: Button컴포넌트 label제거 및 children추가

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,44 @@
+import * as S from "./style";
+
+interface ButtonInterface {
+  buttonType?: string;
+  width?: string;
+  height?: string;
+  isRound?: boolean;
+  isDisabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonInterface> = ({
+  children,
+  buttonType,
+  width,
+  height,
+  isRound,
+  onClick,
+  isDisabled
+}) => {
+  return (
+    <S.Button
+      buttonType={buttonType}
+      width={width}
+      height={height}
+      isRound={isRound}
+      onClick={onClick ? onClick : null}
+      disabled={isDisabled}
+    >
+      {children}
+    </S.Button>
+  );
+};
+
+Button.defaultProps = {
+  buttonType: "red",
+  width: "100%",
+  height: "40",
+  isRound: false,
+  isDisabled: false
+};
+
+export default Button;
