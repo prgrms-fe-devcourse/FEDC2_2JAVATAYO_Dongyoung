17078fbacfb0af879020f65f5273211e112cbf16
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-06-13 17:22:28 +0900

    ✨ feat: Footer 컴포넌트 추가 (#90)
    * ✨ feat: Footer 추가
    
    * 💄 style: 스타일 수정
    
    - resetCSS로 인해 변경된 부분 수정
    
    * ♻️ refactor: 태그명 변경

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import Icon from "../Icon";
+import githubIcon from "../../../assets/icons/icon_github.svg";
+import youtubeIcon from "../../../assets/icons/icon_youtube.svg";
+import * as S from "./style";
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <S.Wrap>
+        <S.Title>2022 2JAVATAYO</S.Title>
+        <S.Content>
+          (주) HARDY PLANT / 공동대표 박유현, 정현진, 김남경, 주형진, 마혜경 /
+          위치 서울 특별시 서초구 강남대로 337,2층 프로그래머스(서초동,
+          대륭서초타워 / 고객센터 010-9858-0000
+        </S.Content>
+        <p>
+          <S.PrivacyPolicy>개인정보 처리방침</S.PrivacyPolicy>
+          <a>이용약관</a>
+        </p>
+        <div>
+          <S.SocialLink>
+            <img src={githubIcon} alt="github 링크" />
+            <img src={youtubeIcon} alt="youTube 발표 영상" />
+          </S.SocialLink>
+        </div>
+      </S.Wrap>
+    </S.Footer>
+  );
+};
+
+export default Footer;
