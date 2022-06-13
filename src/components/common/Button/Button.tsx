b57881b4a01d03a973f6c9c191711d7395107c75
hwoo3303 <hwoo3303@naver.com>
2022-06-13 23:28:00 +0900

    ✨ feat: Button 컴포넌트 옵셔널처리, 구조분해 할당으로 변경

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,25 +1,33 @@
 import * as S from "./style";
 interface ButtonInterface {
-  buttonType: string;
-  width: string;
-  height: string;
-  isRound: boolean;
-  isDisabled: boolean;
-  label: string;
+  buttonType?: string;
+  width?: string;
+  height?: string;
+  isRound?: boolean;
+  isDisabled?: boolean;
+  label?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<ButtonInterface> = (props) => {
+const Button: React.FC<ButtonInterface> = ({
+  buttonType,
+  width,
+  height,
+  isRound,
+  onClick,
+  isDisabled,
+  label
+}) => {
   return (
     <S.Button
-      buttonType={props.buttonType}
-      width={props.width}
-      height={props.height}
-      isRound={props.isRound}
-      onClick={props.onClick ? props.onClick : null}
-      disabled={props.isDisabled}
+      buttonType={buttonType}
+      width={width}
+      height={height}
+      isRound={isRound}
+      onClick={onClick ? onClick : null}
+      disabled={isDisabled}
     >
-      {props.label}
+      {label}
     </S.Button>
   );
 };
