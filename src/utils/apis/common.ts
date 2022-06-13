f2e530d2340cfe7991e447b58474dead1dec7f4a
YuHyun <puh0128@gmail.com>
2022-06-13 11:22:05 +0900

    ♻️ refactor: proxy 제거

--- src/utils/apis/common.ts
+++ src/utils/apis/common.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import storage from "../storage";
 
-// CORS 풀리면 baseURL로 변경
 const baseURL = "http://kdt.frontend.2nd.programmers.co.kr:5005";
 
-const unauthRequest = axios.create({});
-unauthRequest.interceptors.response.use(
-  (config) => config,
-  (error) => Promise.reject(error.response)
-);
-const authRequest = axios.create({});
+const unauthRequest = axios.create({
+  baseURL
+});
+
+const authRequest = axios.create({
+  baseURL
+});
 authRequest.interceptors.request.use((config) => {
   config.headers.authorization = "Bearer " + storage.getItem("TOKEN", "");
   return config;
