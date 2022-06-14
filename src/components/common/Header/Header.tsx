dafdc22c7e8685b6bd35f9ccec9a602797b37192
Jenet <95457808+hyunjin0910@users.noreply.github.com>
2022-06-15 04:11:32 +0900

    ✨ feat: Header 컴포넌트 구현 (임시) (#94)
    * ✨ feat: Header 컴포넌트 구현 (임시)
    
    * 💄 style: storybook args 추가
    
    * ♻️ refactor: 페이지 이동 navigate로 수정
    
    * 💄style: 로그인 상태 class 대신 styled 파일로 변경
    
    * 🔧chore: common 폴더로 이동
    
    * ✨ feat: 로그인시 confirm 받고 메인으로 돌아가기

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,46 @@
+import * as S from "./style";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../../assets/logos/Logo.svg";
+interface HeaderInterface {
+  isLoggedIn?: boolean;
+  userName?: string;
+}
+
+const Header: React.FC<HeaderInterface> = ({
+  isLoggedIn = false,
+  userName = ""
+}) => {
+  const navigate = useNavigate();
+  return (
+    <S.Header>
+      <img
+        src={Logo}
+        alt="Logo"
+        onClick={
+          isLoggedIn
+            ? () => {
+                window.confirm("메인으로 돌아가시겠습니까?");
+                navigate("/");
+              }
+            : () => navigate("/")
+        }
+      />
+      <div>
+        {isLoggedIn ? (
+          <S.LoggedIn>
+            <div>새글쓰기</div>
+            <div>알람</div>
+            <div>프로필</div>
+            <div>{userName}님</div>
+          </S.LoggedIn>
+        ) : (
+          <S.Loggedout onClick={() => console.log("navigate")}>
+            로그인 / 회원가입
+          </S.Loggedout>
+        )}
+      </div>
+    </S.Header>
+  );
+};
+
+export default Header;
