b14b1d8ca5118909c6890a5923359ead86b34ee3
Hyevvy <hyeg0@naver.com>
2022-06-15 23:49:32 +0900

    :bug: fix: antd font-size 14px로 수정

--- src/components/create/SelectBox/style.tsx
+++ src/components/create/SelectBox/style.tsx
@@ -3,8 +3,10 @@ import styled from "@emotion/styled";
 export const SelectBox = styled.div`
   padding: 5px;
   margin: 10px 0;
+  font-size: 14px;
 `;
 
 export const Label = styled.div`
   margin: 10px 0;
+  font-size: 14px;
 `;
