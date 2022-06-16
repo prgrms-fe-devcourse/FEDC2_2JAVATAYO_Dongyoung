b507ef923b65a852cf41538540756f6486d30a1e
NamgyungKim <namgyung.kim@gmail.com>
2022-06-16 15:06:56 +0900

    ♻️ refactor : icon 컴포넌트 삭제

--- src/components/common/Icon/style.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Icon = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`;
--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,6 +1,5 @@
-import Icon from "./Icon";
 import ProfileImage from "./ProfileImage";
 import SkillIcon from "./SkillIcon";
 import ImageUpload from "./ImageUpload";
 
-export { Icon, ProfileImage, SkillIcon, ImageUpload };
+export { ProfileImage, SkillIcon, ImageUpload };
