182552a354069402e7f2b78d4b74154d9056a84e
NamgyungKim <namgyung.kim@gmail.com>
2022-06-17 14:57:15 +0900

    ✨ feat: email, pw 수정

--- src/components/common/TempLogin/index.tsx
+++ src/components/common/TempLogin/index.tsx
@@ -2,20 +2,39 @@ import styled from "@emotion/styled";
 import { authAPI } from "@utils/apis";
 import storage from "@utils/storage";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 const TempLogin = () => {
   const [isLogin, setLogin] = useState(false);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState("");
 
   const logIn = async (num) => {
-    const { data } = await authAPI.signIn({
-      email: `user${num}@gmail.com`,
-      password: "1234"
-    });
+    const user = [
+      {
+        fullName: "user1",
+        email: "user1@gmail.com",
+        password: "1234"
+      },
+      {
+        fullName: "user2",
+        email: "user2@gmail.com",
+        password: "1234"
+      },
+      {
+        fullName: "knk",
+        email: "knk@gmail.com",
+        password: "1234"
+      },
+      {
+        fullName: "유현naver",
+        email: "puh0128@naver.com",
+        password: "1234"
+      }
+    ];
+
+    const { data } = await authAPI.signIn(user[num]);
     storage.setItem("TOKEN", data.token);
     setLogin(true);
-    setUser(num);
+    setUser(user[num].fullName);
   };
 
   const logOut = () => {
@@ -33,21 +52,15 @@ const TempLogin = () => {
     <>
       {isLogin ? (
         <div>
-          <button>
-            <Link to={"/profile/62a0cf0bc882bf3a287f907c"}>profile페이지</Link>
-          </button>
           <button onClick={logOut}>로그아웃</button>
-          user{user}
+          {user}
         </div>
       ) : (
         <Form>
-          <button>
-            <Link to={"/profile/62a0cf0bc882bf3a287f907c"}>profile페이지</Link>
-          </button>
-          <button onClick={() => logIn(1)}>user1</button>
-          <button onClick={() => logIn(2)}>user2</button>
-          <button onClick={() => logIn(3)}>user3</button>
-          <button onClick={() => logIn(4)}>user4</button>
+          <button onClick={() => logIn(0)}>user1</button>
+          <button onClick={() => logIn(1)}>user2</button>
+          <button onClick={() => logIn(2)}>knk</button>
+          <button onClick={() => logIn(3)}>유현</button>
         </Form>
       )}
     </>
