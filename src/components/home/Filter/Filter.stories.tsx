897f8375a9a022eaaccda199e3e611fd5570b88b
NamgyungKim <namgyung.kim@gmail.com>
2022-06-17 16:37:53 +0900

    💄 style: 변수명 변경

--- src/components/home/Filter/Filter.stories.tsx
+++ src/components/home/Filter/Filter.stories.tsx
@@ -3,7 +3,6 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Filter from "./Filter";
 import reset from "../../../styles/reset";
 import { useState } from "react";
-import Card from "@components/common/Card";
 
 export default {
   title: "Page/Filter",
@@ -12,14 +11,13 @@ export default {
 } as ComponentMeta<typeof Filter>;
 
 export const Default: ComponentStory<typeof Filter> = () => {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   return (
     <>
       <Global styles={reset} />
-      <Filter setPost={setPost}></Filter>
-      {JSON.stringify(post)}
-      <Card post={post[0]} />
+      <Filter setPost={setPosts}></Filter>
+      {JSON.stringify(posts)}
     </>
   );
 };
