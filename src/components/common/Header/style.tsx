b1cd4025875e42db4d85de59c05a048d205115a4
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-06-17 19:58:35 +0900

    💄 style: header 스타일 변경 (#172)
    💄 style: header 디자인

--- src/components/common/Header/style.tsx
+++ src/components/common/Header/style.tsx
@@ -1,8 +1,25 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import theme from "../../../../src/styles/theme";
+
+const dot = css`
+  position: absolute;
+  top: -2px;
+  right: -2px;
+  display: block;
+  border: 2px solid #fff;
+  border-radius: 50%;
+  width: 12px;
+  height: 12px;
+  background-color: ${theme.$red};
+  content: "";
+`;
 
 export const Header = styled.header`
-  padding: 18px 61px;
-  display: inline-flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
   height: 60px;
   font-weight: bolder;
   background-color: #ffffff;
@@ -12,12 +29,73 @@ export const Header = styled.header`
   justify-content: space-between;
   box-sizing: border-box;
   z-index: 10000;
+
+  svg {
+    width: 180px;
+    cursor: pointer;
+  }
+
+  h1 {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    border: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    clip-path: inset(50%);
+  }
+
+  @media (max-width: 700px) {
+    padding: 30px;
+  }
 `;
 
 export const LoggedIn = styled.div`
-  min-width: 200px;
-  display: flex;
+  display: inline-flex;
+  align-items: center;
   justify-content: space-between;
+  a,
+  span {
+    font-size: 16px;
+  }
+  a {
+    margin-left: 15px;
+    :hover {
+      color: ${theme.$gray600};
+    }
+  }
 `;
 
-export const Loggedout = styled.div``;
+export const Notice = styled.div<{ isGetAlarm: boolean }>`
+  position: relative;
+  border: none;
+  margin: 0 20px;
+  background-color: #fff;
+  cursor: pointer;
+  svg {
+    width: 20px;
+  }
+  ::after {
+    ${({ isGetAlarm }) => (isGetAlarm ? dot : null)}
+  }
+`;
+
+export const LoggedOut = styled.a`
+  font-size: 16px;
+  line-height: 16px;
+`;
+
+export const UserFullName = styled.span`
+  margin-left: 12px;
+`;
+
+export const User = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  img {
+    cursor: pointer;
+  }
+`;
