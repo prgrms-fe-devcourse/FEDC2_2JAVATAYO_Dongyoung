c235ecdac7368405b156cb5b6b98de5d0c54c08e
YuHyun <puh0128@gmail.com>
2022-06-18 16:57:26 +0900

    💄 style: 프로필 이미지 변경 아이콘 수정(연필 -> 톱니바퀴)

--- src/components/profile/ProfileImageBox/index.tsx
+++ src/components/profile/ProfileImageBox/index.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import { ProfileImage, ImageUpload } from "@components/common";
-import { EditIcon } from "@components/profile";
+import { ReactComponent as SettingComponent } from "@assets/icons/icon_setting.svg";
 import { userAPI } from "@utils/apis";
 import * as S from "./style";
 
@@ -31,7 +31,12 @@ const ProfileImageBox: FC<ProfileImageBoxInterface> = ({ isMine, imgSrc }) => {
       {isMine ? (
         <ImageUpload
           onImageUpload={handleImageUpload}
-          replacement={<EditIcon right="-3px" />}
+          replacement={<SettingComponent width="20px" height="20px" />}
+          replacementStyle={{
+            position: "absolute",
+            bottom: "0",
+            right: "-3px"
+          }}
         />
       ) : null}
     </S.ProfileImageWrapper>
