be565a8b7b28ee97e8dc19c0f51c1f4c5fa61a72
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-06-20 04:34:08 +0900

    ✨ feat: header에 DropDown 추가 (#207)

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -6,16 +6,20 @@ import { useEffect, useState } from "react";
 import storage from "@utils/storage";
 import { Link } from "react-router-dom";
 import TempLogin from "../TempLogin";
-import { authAPI } from "@utils/apis";
+import { authAPI, notificationAPI } from "@utils/apis";
 import ProfileImage from "../ProfileImage";
+import DropDown from "../DropDown";
+import { useAuth } from "@contexts/AuthProvider";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
+  const { onLogOut } = useAuth();
 
   //이후 contextAPI로 변경
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userData, setUserData] = useState({ fullName: "test" });
-  const { fullName } = userData;
+  const [userData, setUserData] = useState({ fullName: "test", _id: "" });
+  const [noticeContents, setNoticeContents] = useState([]);
+  const { fullName, _id } = userData;
   const userInfo = async () => {
     const { data } = await authAPI.checkAuthUser();
     setUserData(data);
@@ -25,10 +29,31 @@ const Header: React.FC = () => {
   }, [isLoggedIn]);
   //이후 contextAPI로 변경
 
+  const logOut = () => {
+    storage.removeItem("TOKEN");
+    onLogOut();
+  };
+
+  const notice = async () => {
+    const { data } = await notificationAPI.getNotificationList();
+    console.log(data);
+  };
+
+  const subNav = [
+    {
+      label: "마이페이지",
+      event: () => navigate(`/profile/${_id}`)
+    },
+    {
+      label: "로그아웃",
+      event: () => logOut()
+    }
+  ];
+
   useEffect(() => {
     const hasToken = storage.getItem("TOKEN", false) ? true : false;
     setIsLoggedIn(hasToken);
-    alert;
+    notice();
   }, []);
 
   return (
@@ -43,13 +68,17 @@ const Header: React.FC = () => {
       {isLoggedIn ? (
         <S.LoggedIn>
           <Link to={"/create"}>새글쓰기</Link>
-          <S.Notice isGetAlarm={true} onClick={() => alert("알림")}>
-            <Bell />
-          </S.Notice>
-          <S.User onClick={() => alert("사용자 토글매뉴")}>
-            <ProfileImage size="sm" />
-            <S.UserFullName>{fullName} 님</S.UserFullName>
-          </S.User>
+          <DropDown contents={noticeContents}>
+            <S.Notice isGetAlarm={noticeContents.length !== 0}>
+              <Bell />
+            </S.Notice>
+          </DropDown>
+          <DropDown contents={subNav}>
+            <S.User>
+              <ProfileImage size="sm" />
+              <S.UserFullName>{fullName} 님</S.UserFullName>
+            </S.User>
+          </DropDown>
         </S.LoggedIn>
       ) : (
         <S.LoggedOut onClick={() => navigate("/signin")}>
