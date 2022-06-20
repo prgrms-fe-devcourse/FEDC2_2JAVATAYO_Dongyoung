35c2ab80ed6740042ebc335366a7a46ac93f68bd
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-06-21 00:44:51 +0900

    :lipstick: style: 수정페이지 css 작업 (#237)

--- src/pages/edit/style.tsx
+++ src/pages/edit/style.tsx
@@ -4,7 +4,9 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 10px;
   font-size: 14px;
+  margin: 10px 0;
   label {
     margin: 5px 0;
   }
@@ -12,4 +14,17 @@ export const Wrapper = styled.div`
 
 export const InnerWrapper = styled.div`
   width: 50%;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin: auto;
+`;
+
+export const H3 = styled.h3`
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const DivWrapper = styled.div`
+  margin: 10px 0;
 `;
