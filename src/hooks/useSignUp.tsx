0a54b7032ce8bf1ed8b0b37480beaaaa78880687
Jenet <xuanchen0910@gmail.com>
2022-06-20 16:05:01 +0900

    🐛 fix: 닉네임 중복조건을 확인해야 가입 가능

--- src/hooks/useSignUp.tsx
+++ src/hooks/useSignUp.tsx
@@ -17,6 +17,7 @@ const useSignUp = ({ initialValues, isError }) => {
   const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState<error>({});
   const [isLoading, setIsLoading] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -31,30 +32,32 @@ const useSignUp = ({ initialValues, isError }) => {
     if (Object.keys(newErrors).length !== 0) {
       setErrors(newErrors);
     } else {
-      await authAPI
-        .signUp({
-          email: values.email,
-          fullName: values.fullName,
-          password: values.password
-        })
-        .then((res) => {
-          const { user, token } = res.data;
-          onLogin(user);
-          storage.setItem("TOKEN", token);
-          navigate("/");
-        })
-        .catch((err) => {
-          if (err.response.status === 400) {
-            alert("입력하신 정보를 확인해주세요");
-            setValues({
-              fullName: "",
-              email: "",
-              password: "",
-              passwordConfirm: ""
+      isChecked === false
+        ? alert("닉네임 중복확인을 해주세요")
+        : await authAPI
+            .signUp({
+              email: values.email,
+              fullName: values.fullName,
+              password: values.password
+            })
+            .then((res) => {
+              const { user, token } = res.data;
+              onLogin(user);
+              storage.setItem("TOKEN", token);
+              navigate("/");
+            })
+            .catch((err) => {
+              if (err.response.status === 400) {
+                alert("입력하신 정보를 확인해주세요");
+                setValues({
+                  fullName: "",
+                  email: "",
+                  password: "",
+                  passwordConfirm: ""
+                });
+                setErrors({});
+              }
             });
-            setErrors({});
-          }
-        });
       setIsLoading(false);
     }
   };
@@ -64,9 +67,10 @@ const useSignUp = ({ initialValues, isError }) => {
     const names = response.data.map((x) => x.fullName);
     if (isError(values).fullName === undefined) {
       if (names.includes(values.fullName)) {
-        alert("이미 있는 이름입니다.");
         setValues({ fullName: "" });
+        alert("이미 있는 이름입니다.");
       } else {
+        setIsChecked(true);
         alert("사용가능한 이름입니다.");
       }
     }
