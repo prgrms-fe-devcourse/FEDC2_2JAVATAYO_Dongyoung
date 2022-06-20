5667f329497f578c005b999c7ab34683630d7a4d
Hyevvy <hyeg0@naver.com>
2022-06-20 22:56:20 +0900

    :sparkles: feat: email 유효성 검사 및 textArea ref 연결

--- src/pages/create/create.tsx
+++ src/pages/create/create.tsx
@@ -47,17 +47,27 @@ const Create: React.FC = () => {
   const [alertMessage, setAlertMessage] = useState("메인페이지로 이동합니다");
   const titleRef = useRef(null);
   const emailRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  const validateEmail = (email) => {
+    const re =
+      /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    console.log(re.test(email));
+    return re.test(email);
+  };
 
   const handleCreate = () => {
     if (_title === "") {
       titleRef.current.focus();
       return;
-    } else if (email === "") {
+    } else if (email === "" || !validateEmail(email)) {
       emailRef.current.focus();
+      return;
+    } else if (introduction.length < 2) {
+      textAreaRef.current.focus();
       return;
     }
 
-    //TODO : 형진님의 TextArea focus 추가
     //TODO : blocker.js 멘트 수정?
     parts.forEach((part) => {
       const currentChannelId = CHANNELS[part.channel]._id;
@@ -124,6 +134,9 @@ const Create: React.FC = () => {
         <S.InnerWrapper>
           <Label>이메일</Label>
           <Input
+            pattern=".+@globex\.com"
+            type="email"
+            ref={emailRef}
             placeholder="이메일을 입력해주세요"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -163,6 +176,7 @@ const Create: React.FC = () => {
       <Textarea
         isIntroduction={true}
         isLogin={true}
+        textAreaRef={textAreaRef}
         onChange={(e) => setIntroduction(e.target.value)}
       >
         {introduction}
