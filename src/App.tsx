1649c0184ad1b704e079555c36f90f988a983dc9
김남경 <namgyung.kim@metarock.co.kr>
2022-11-08 15:15:36 +0900

    Revert "✨feat: 이스터애그 추가"
    This reverts commit 39dc223fe4c7395130726f25d32baf9f65278e33.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,7 @@ import { Global, ThemeProvider } from "@emotion/react";
 import reset from "./styles/reset";
 import theme from "./styles/theme";
 import AuthProvider from "./contexts/AuthProvider";
-
-const consoleLog = `쾅쾅쾅쾅쾅쾅쾅쾅쾅
-쾅쾅         쾅쾅
-쾅쾅（∩8ㅁ8）쾅쾅
-  ＿/_ﾐつ/￣￣￣/
-    ＼/＿＿＿/`;
-
 const App: React.FC = () => {
-  console.log(consoleLog);
   return (
     <div>
       <ThemeProvider theme={theme}>
